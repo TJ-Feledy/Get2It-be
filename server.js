dbf3d5b6e670df7a1e5699663490ca676e1922b3
TJ-Feledy <teej387@yahoo.com>
2020-06-05 18:53:15 -0500

    Readded api to server

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ server.use(helmet());
 server.use(express.json());
 server.use(cors());
 
-server.use('/auth', authRouter);
-server.use('/users', usersRouter);
+server.use('/api/auth', authRouter);
+server.use('/api/users', usersRouter);
 
 server.get('/', (req, res) => {
   res.send("It's alive!");
