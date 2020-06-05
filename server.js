53a0777d707619443621b6a6dda61997040dfb8c
TJ-Feledy <teej387@yahoo.com>
2020-06-05 18:45:48 -0500

    Removed "api" from servers

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ server.use(helmet());
 server.use(express.json());
 server.use(cors());
 
-server.use('/api/auth', authRouter);
-server.use('/api/users', usersRouter);
+server.use('/auth', authRouter);
+server.use('/users', usersRouter);
 
 server.get('/', (req, res) => {
   res.send("It's alive!");
